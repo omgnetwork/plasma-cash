31c8a4ea7d70435852a6f75e43aecb92e7cef49f
Boolafish <boolafish945@gmail.com>
2018-04-16 22:56:18 +0900

    unit test for merkle tree

--- plasma_cash/utils/merkle/sparse_merkle_tree.py
+++ plasma_cash/utils/merkle/sparse_merkle_tree.py
@@ -1,41 +1,52 @@
 from collections import OrderedDict
+
 from ethereum.utils import sha3
 
 
 class SparseMerkleTree(object):
 
-    def __init__(self, leaves={}):
-        self.depth = 256
+    def __init__(self, depth=256, leaves={}):
+        self.depth = depth
+        if len(leaves) > 2**(depth-1):
+            raise self.TreeSizeExceededException(
+                'tree with depth {} could not have {} leaves'.format(depth, len(leaves))
+            )
+
         # Sort the transaction dict by index.
         self.leaves = OrderedDict(sorted(leaves.items(), key=lambda t: t[0]))
-        self.default_nodes = [b'\x00' * 32]
-        self.create_default_nodes()
+        self.default_nodes = self.create_default_nodes(self.depth)
         if leaves:
-            self.root = self.create_tree()
+            self.root = self.create_tree(self.leaves, self.depth, self.default_nodes)
         else:
             self.root = self.default_nodes[self.depth - 1]
 
-    def create_default_nodes(self):
+    def create_default_nodes(self, depth):
         # Default nodes are the nodes whose children are both empty nodes at each level.
-        for level in range(1, self.depth):
-            prev_default = self.default_nodes[level - 1]
-            self.default_nodes.append(sha3(prev_default + prev_default))
+        default_nodes = [b'\x00' * 32]
+        for level in range(1, depth):
+            prev_default = default_nodes[level - 1]
+            default_nodes.append(sha3(prev_default + prev_default))
+        return default_nodes
 
-    def create_tree(self):
-        tree_level = self.leaves
-        for level in range(self.depth - 1):
+    def create_tree(self, ordered_leaves, depth, default_nodes):
+        tree_level = ordered_leaves
+        for level in range(depth - 1):
             next_level = {}
             prev_index = -1
             for index, value in tree_level.items():
                 if index % 2 == 0:
                     # If the node is a left node, assume the right sibling is a default node.
-                    next_level[index // 2] = sha3(value + self.default_nodes[level])
+                    # in the case right sibling is not default node, it would override on next round
+                    next_level[index // 2] = sha3(value + default_nodes[level])
                 else:
                     # If the node is a right node, check if its left sibling is a default node.
                     if index == prev_index + 1:
                         next_level[index // 2] = sha3(tree_level[prev_index] + value)
                     else:
-                        next_level[index // 2] = sha3(self.default_nodes[level] + value)
+                        next_level[index // 2] = sha3(default_nodes[level] + value)
                 prev_index = index
             tree_level = next_level
         return tree_level[0]
+
+    class TreeSizeExceededException(Exception):
+        """there are too many leaves for the tree to build"""
---
+++ unit_tests/utils/merkle/test_sparse_merkle_tree.py
@@ -0,0 +1,42 @@
+import pytest
+from ethereum.utils import sha3
+
+from plasma_cash.utils.merkle.sparse_merkle_tree import SparseMerkleTree
+
+
+class TestSparseMerkleTree(object):
+
+    def test_all_leaves_with_val(self):
+        dummy_val = b'\x01' * 32
+        leaves = {0: dummy_val, 1: dummy_val, 2: dummy_val, 3: dummy_val}
+        tree = SparseMerkleTree(depth=3, leaves=leaves)
+        mid_level_val = sha3(dummy_val + dummy_val)
+        assert tree.root == sha3(mid_level_val + mid_level_val)
+
+    def test_empty_leaves(self):
+        tree = SparseMerkleTree(depth=3)
+        empty_val = b'\x00' * 32
+        mid_level_val = sha3(empty_val + empty_val)
+        assert tree.root == sha3(mid_level_val + mid_level_val)
+
+    def test_empty_left_leave(self):
+        empty_val = b'\x00' * 32
+        dummy_val = b'\x01' * 32
+        leaves = {1: dummy_val, 2: dummy_val, 3: dummy_val}
+        tree = SparseMerkleTree(depth=3, leaves=leaves)
+        mid_left_val = sha3(empty_val + dummy_val)
+        mid_right_val = sha3(dummy_val + dummy_val)
+        assert tree.root == sha3(mid_left_val + mid_right_val)
+
+    def test_empty_right_leave(self):
+        empty_val = b'\x00' * 32
+        dummy_val = b'\x01' * 32
+        leaves = {0: dummy_val, 2: dummy_val, 3: dummy_val}
+        tree = SparseMerkleTree(depth=3, leaves=leaves)
+        mid_left_val = sha3(dummy_val + empty_val)
+        mid_right_val = sha3(dummy_val + dummy_val)
+        assert tree.root == sha3(mid_left_val + mid_right_val)
+
+    def test_exceed_tree_size(self):
+        with pytest.raises(SparseMerkleTree.TreeSizeExceededException):
+            SparseMerkleTree(depth=1, leaves={0: '0', 1: '1'})
