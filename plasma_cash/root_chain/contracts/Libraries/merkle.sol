f1cf155570b3b204523c01723c23405694769e97
bun919tw <bun919tw@gmail.com>
2018-05-16 01:52:54 +0000

    Add start_exit feature.

---
+++ plasma_cash/root_chain/contracts/Libraries/merkle.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+/**
+ * @title Merkle
+ * @dev Checks that a particular leaf node is in a given Merkle tree given the index, root hash, and a proof
+ */
+
+library Merkle {
+    function checkMembership(bytes32 leaf, uint256 index, bytes32 rootHash, bytes proof)
+        internal
+        pure
+        returns (bool)
+    {
+        bytes32 proofElement;
+        bytes32 computedHash = leaf;
+
+        for (uint256 i = 32; i <= proof.length; i += 32) {
+            assembly {
+                proofElement := mload(add(proof, i))
+            }
+            if (index % 2 == 0) {
+                computedHash = keccak256(computedHash, proofElement);
+            } else {
+                computedHash = keccak256(proofElement, computedHash);
+            }
+            index = index / 2;
+        }
+        return computedHash == rootHash;
+    }
+}
--- plasma_cash/root_chain/contracts/RootChain/RootChain.sol
+++ plasma_cash/root_chain/contracts/RootChain/RootChain.sol
@@ -1,7 +1,15 @@
 pragma solidity ^0.4.18;
 
+import 'merkle.sol';
+import 'RLP.sol';
+
 
 contract RootChain {
+    using Merkle for bytes32;
+    using RLP for bytes;
+    using RLP for RLP.RLPItem;
+    using RLP for RLP.Iterator;
+
     /*
      * Events
      */
@@ -15,6 +23,7 @@ contract RootChain {
     uint public currentBlkNum;
     mapping(uint => bytes32) public childChain;
     mapping(bytes32 => uint) public wallet;
+    mapping(uint => uint) public exits;
 
     /*
      * Modifiers
@@ -56,5 +65,40 @@ contract RootChain {
         wallet[uid] = amount;
         depositCount += 1;
         Deposit(msg.sender, amount, uint256(uid));
+    }
+
+    function startExit(
+        bytes prevTx,
+        bytes prevTxProof,
+        uint prevTxBlkNum,
+        bytes tx,
+        bytes txProof,
+        uint txBlkNum
+    )
+        public
+    {
+        var prevTxList = prevTx.toRLPItem().toList();
+        var txList = tx.toRLPItem().toList();
+        require(prevTxList.length == 5);
+        require(txList.length == 5);
+
+        // TODO: Should optimize these lines with more readability.
+        // TODO: Should check the transaction signature.
+        require(prevTxBlkNum == txList[0].toUint());
+        require(prevTxList[1].toUint() == txList[1].toUint());
+        require(txList[2].toUint() == txList[2].toUint());
+        require(msg.sender == txList[3].toAddress());
+        uint uid = txList[1].toUint();
+
+        bytes32 prevMerkleHash = sha3(prevTx);
+        bytes32 prevRoot = childChain[prevTxBlkNum];
+        bytes32 merkleHash = sha3(tx);
+        bytes32 root = childChain[txBlkNum];
+        require(prevMerkleHash.checkMembership(uid, prevRoot, prevTxProof));
+        require(merkleHash.checkMembership(uid, root, txProof));
+
+        // Record the exitable timestamp.
+        require(exits[uid] == 0);
+        exits[uid] = block.timestamp + 2 weeks;
     }
 }
